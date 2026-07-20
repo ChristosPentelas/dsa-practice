21f3858309525a3490ea65522d808de3b4b23253
ChristosPentelas <xristos.pentel@gmail.com>
2026-07-20 19:57:21 +0300

    Complexity is O(log n) time and O(1) space. Easy level.

--- Arrays_practice/SearchInsertPosition.java
+++ Arrays_practice/SearchInsertPosition.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SearchInsertPosition {
     public static void main(String[] args) {
         int[] nums = {1,3,5,6};
-        int target = 4;
+        int target = 2;
 
         int index = Arrays.binarySearch(nums, target);
         System.out.println(index>=0 ? index : -index-1);
